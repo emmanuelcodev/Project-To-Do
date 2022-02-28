0d5be304d860e5625c533cd8541658709ddb51a3
emmanuelcodev <emmanuel.adw@gmail.com>
2022-02-27 20:33:58 -0800

    added basic to do list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+//import logo from './logo.svg';
+import {sites} from './data/data'
 import './App.css';
+import './css/index.css'
+
 
 function App() {
+  const [websites, setWebsites] = useState(sites);
+
+  const removeItem = (sid)=> {
+    const reducedList = websites.filter((site)=> site.id !== sid)
+    setWebsites(reducedList)
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  <div className="container">
+    <h1>Links</h1>
+     {
+      websites.map((site) => {
+        const sid = site.id
+        return (
+        <div >
+          
+          <a href={site.url} target="_blank" >
+            <div className='site'>
+            <img src={site.img_url} alt={site.name} />
+           
+            <p className = 'inlineP'>{site.name}</p>
+             </div>
+          </a>
+         
+          <button onClick={()=>removeItem(sid)}>Remove</button>
+          
+        </div>
+        )
+       }
+    
+      )
+        
+    }
+      </div>
+  
+  )
+  }
 
 export default App;
